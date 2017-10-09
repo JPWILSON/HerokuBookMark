aac6d7d990e80fa7af03128c220d31d12419277b
Jean-Paul Wilson <jeanpaulwilson@gmail.com>
2017-10-09 16:01:04 -0500

    Add readme and add intro to homepage

--- BookMarkApp.py
+++ BookMarkApp.py
@@ -9,9 +9,12 @@ blnk_form = """
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>JPs BookMark App</title>
 </head>
 <body>
+<h2>J-P's BookMark site</h2>
+<hr>
+<p>Sure, its pretty useless in that it doesn't have a backend, so there is no persistent data storage... yet.</p>
 	<form action = "http://localhost:8000" method = "POST">
 		<table>
 			<tr>
---
+++ README.md
@@ -0,0 +1,6 @@
+## Bookmark Project	
+
+This is a simple server that allows one to make keywords for their main urls, and typing in the keyword will take 
+one to the url. It is useful for remembering long urls. 
+Basically, its a bookmark, already built into most web browsers. I am just making one so I 
+know how it works, as I learn web dev... 
